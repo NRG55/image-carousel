f8c09caa90244450388a2067fe7ec3d3b24463dc
NRG55 <maloj_1980@yahoo.fr>
2024-07-14 19:03:43 +0100

    Index.html - added div round buttons container (with 3 buttons inside). Index.js - slide function is updated. Style.css - is done.

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 const images = document.querySelectorAll("[data-image]");
 const buttonNext = document.getElementById("button-next");
 const buttonPrevious = document.getElementById("button-previous");
+const roundButtons = document.querySelectorAll(".round-buttons-container > button");
 const slider = document.querySelector(".slider");
-let slideDistance = 0;
+let currentIndex = 0;
 
-images.forEach((div) => {
-    console.log(div)
-    console.log(div.getAttribute("data-image"))
+images.forEach((div) => {   
     div.style.backgroundImage = `url(./images/${div.getAttribute("data-image")})`;
 });
 
-const slide = (direction) => {
-    const imageSizeInPercent = (1 / images.length) * 100;   
+const slide = (index) => {   
+    if (index < 0) {
+        index = 0;        
+    };
 
-    if (direction === "previous") {
-        if (slideDistance < 0 || slideDistance === 0) {                
-            slideDistance = 0;
-            return
-            };
-
-        slideDistance -= imageSizeInPercent;
-        slider.style.transform = `translateX(-${slideDistance}%)`;
-        
-        console.log(slideDistance)
-        return;  
-    } 
-
-    slideDistance += imageSizeInPercent;
-    slider.style.transform = `translateX(-${slideDistance}%)`;       
+    if (index >= images.length - 1) {
+        index = images.length - 1;        
+    };    
+    
+    roundButtons[currentIndex].classList.remove("active");
+    roundButtons[index].classList.add("active");
+    slider.style.transform = `translateX(-${(index / images.length) * 100}%)`;
+    currentIndex = index;   
 };
 
-buttonNext.onclick = () => {   
-    slide();
+buttonNext.onclick = () => {     
+    slide(currentIndex + 1);
 };
 
 buttonPrevious.onclick = () => {   
-    slide("previous");
+    slide(currentIndex - 1);
 };
+
+roundButtons.forEach((button, index) => {   
+    button.addEventListener("click", () => {      
+        slide(index);
+    });
+});
+
+roundButtons[0].classList.add("active");
+
+
 
 
